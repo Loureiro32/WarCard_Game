376b15718bbe3be6ee3bf358b5687518dfa0713a
Loureiro <mindera@C02Z689TLVCF-ME00547.local>
2024-12-23 12:19:55 +0000

    Add Initial Menu

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,29 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Game {
+    public static final String PLAY_COLOR = "\u001B[32m";
+    public static final String EXIT_COLOR = "\u001B[31m";
+    public static final String RESET_COLOR = "\u001B[0m";
+
     List<Players> playersList = new ArrayList<Players>();
 
+    public void play(){}
 
+    public void menu(){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println(PLAY_COLOR + " PLAY " +RESET_COLOR + "\n"
+              + EXIT_COLOR + " EXIT" );
+
+        String userInput = input.nextLine().toUpperCase();
+        InitialMenu choice = InitialMenu.valueOf(userInput);
+
+        switch (choice){
+            case PLAY -> play();
+            case EXIT -> System.exit(1);
+        }
+    }
 
 }
---
+++ src/InitialMenu.java
@@ -0,0 +1,4 @@
+public enum InitialMenu {
+    PLAY,
+    EXIT
+}
