6daa33209a5a208ec93b624ae930b4168ef90ebd
Loureiro <mindera@C02Z689TLVCF-ME00547.lan>
2025-01-03 11:07:20 +0000

    giveCards method conclusion

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,7 @@
 import Enums.InitialMenu;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,52 +12,54 @@ public class Game {
 
     protected Player[] player = new Player[2];
 
-    public Game(){
+    public Game() {
         this.player[0] = new Player("Hugo");
         this.player[1] = new Player("Ricardo");
 
     }
 
-    public void startGame(){
+    public void startGame() {
         Player player1 = player[0];
         Player player2 = player[1];
 
-        System.out.println(PLAY_COLOR + player1.getName()+ RESET_COLOR + EXIT_COLOR + " VS "
+        System.out.println(PLAY_COLOR + player1.getName() + RESET_COLOR + EXIT_COLOR + " VS "
                 + RESET_COLOR + PLAY_COLOR + player2.getName() + RESET_COLOR);
 
-        //giveCards(player1, player2);
+        giveCards(player1, player2);
     }
 
 
-    public void giveCards(){
+    public void giveCards(Player player1, Player player2) {
 
         List<Card> cardDeck = new ArrayList<>();
 
-        for(int x = 0; x<13; x++){
-            for(int y = 0; y<4; y++){
-                cardDeck.add(new Card(x,y));
+        for (int x = 0; x < 13; x++) {
+            for (int y = 0; y < 4; y++) {
+                cardDeck.add(new Card(x, y));
             }
         }
 
-        System.out.println(cardDeck.size());//52/26
-        System.out.println(cardDeck);
+        Collections.shuffle(cardDeck);
+        List<Card> deck1 = new ArrayList<>(cardDeck.subList(0, cardDeck.size() / 2));
+        List<Card> deck2 = new ArrayList<>(cardDeck.subList(cardDeck.size() / 2, cardDeck.size()));
 
-        for(int i =0; i<= 52; i++) {
-            int random = (int) (Math.random() * cardDeck.size() - 1) + 1;
-            System.out.println(random);
-        }
+        player1.setCardsList(deck1);
+        player2.setCardsList(deck2);
+
+        System.out.println(PLAY_COLOR + player1.getName() + " --> " + RESET_COLOR + player1.getCardsList());
+        System.out.println(PLAY_COLOR + player2.getName() + " --> " + RESET_COLOR + player2.getCardsList());
     }
 
-    public void menu(){
+    public void menu() {
 
         Scanner input = new Scanner(System.in);
-        System.out.println(PLAY_COLOR + " PLAY " +RESET_COLOR + "\n"
-              + EXIT_COLOR + " EXIT" + RESET_COLOR );
+        System.out.println(PLAY_COLOR + " PLAY " + RESET_COLOR + "\n"
+                + EXIT_COLOR + " EXIT" + RESET_COLOR);
 
         String userInput = input.nextLine().toUpperCase();
         InitialMenu choice = InitialMenu.valueOf(userInput);
 
-        switch (choice){
+        switch (choice) {
             case PLAY -> startGame();
             case EXIT -> System.exit(1);
         }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Game newgame = new Game();
-        newgame.giveCards();
+        newgame.menu();
     }
 }
