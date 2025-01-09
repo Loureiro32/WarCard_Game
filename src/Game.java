91e7b8b01ec1f8d2c7c9935deff05092e6693410
Loureiro <mindera@C02Z689TLVCF-ME00547.local>
2025-01-09 13:29:55 +0000

    Hot Fix

--- src/Game.java
+++ src/Game.java
@@ -6,34 +6,40 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Game {
-    private Player[] player = new Player[2];
+    private Player player1;
+    private Player player2;
     private int roundCounter = 0;
 
     public Game() {
-        this.player[0] = new Player("Hugo");
-        this.player[1] = new Player("Loureiro");
-
+        this.player1 = new Player("Hugo");
+        this.player2 = new Player("Loureiro");
     }
 
-    public void startGame() {
-        Player player1 = player[0];
-        Player player2 = player[1];
+    private void startGame() {
         List<Card> cardDeck = new ArrayList<>();
 
         System.out.println(Colors.GREEN + player1.getName() + Colors.Default + Colors.RED + " VS "
                 + Colors.Default + Colors.GREEN + player2.getName() + Colors.Default);
 
-        try {
-            initialDeck(cardDeck);
-            shuffleDeck(cardDeck);
-            giveCards(player1, player2, cardDeck);
-            round(player1, player2);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        initialDeck(cardDeck);
+        shuffleDeck(cardDeck);
+        giveCards(cardDeck);
+        round(player1, player2);
+    }
+
+    private void initialDeck(List<Card> initialDeck) {
+        for (int x = 2; x < 15; x++) {
+            for (int y = 0; y < 4; y++) {
+                initialDeck.add(new Card(x, y));
+            }
         }
     }
 
-    public void giveCards(Player player1, Player player2, List<Card> initialDeck) {
+    private void shuffleDeck(List<Card> deck) {
+        Collections.shuffle(deck);
+    }
+
+    private void giveCards(List<Card> initialDeck) {
 
         int deckSize = initialDeck.size() / 2;
         List<Card> deck1 = new ArrayList<>(initialDeck.subList(0, deckSize));
@@ -44,35 +50,22 @@ public class Game {
 
         System.out.println(Colors.GREEN + player1.getName() + " DECK -->" + Colors.Default + player1.getCardsList());
         System.out.println(Colors.RED + player2.getName() + " DECK -->" + Colors.Default + player2.getCardsList());
-
     }
 
-    public void initialDeck(List<Card> initialDeck) {
-        for (int x = 2; x < 15; x++) {
-            for (int y = 0; y < 4; y++) {
-                initialDeck.add(new Card(x, y));
-            }
-        }
-    }
-
-    public void shuffleDeck(List<Card> deck) {
-        Collections.shuffle(deck);
-    }
-
-    public void round(Player player1, Player player2) throws InterruptedException {
+    public void round(Player player1, Player player2) {
         while (true) {
 
             System.out.println("Round : " + roundCounter);
             System.out.println(player1.getName() + " place " + player1.faceCard());
             System.out.println(player2.getName() + " place " + player2.faceCard());
-            if (player1.playerCard() > player2.playerCard()) {
+            if (player1.pickCard() > player2.pickCard()) {
                 changeDeckCards(player1, player2);
-            } else if (player2.playerCard() > player1.playerCard()) {
+            } else if (player2.pickCard() > player1.pickCard()) {
                 changeDeckCards(player2, player1);
             }
-//            if (player1.playerCard() == player2.playerCard()) {
-//
-//            }
+            if (player1.pickCard() == player2.pickCard()) {
+                changeDeckCards(player1, player2);
+            }
 
             if (player1.getCardsList().size() <= 3) {
                 System.out.println(player2.getName() + " Wins!");
@@ -82,16 +75,16 @@ public class Game {
                 System.out.println(player1.getName() + " Wins!");
                 break;
             }
-            Thread.sleep(1000);
             roundCounter++;
         }
     }
 
-    public void changeDeckCards(Player roundWinner, Player roundLoser) {
-        roundWinner.getCardsList().addLast(roundWinner.getCardsList().getFirst());
-        roundWinner.getCardsList().removeFirst();
-        roundWinner.getCardsList().addLast(roundLoser.getCardsList().getFirst());
-        roundLoser.getCardsList().removeFirst();
+    public void changeDeckCards(Player winner, Player loser) {
+        winner.getCardsList().addLast(winner.getCardsList().getFirst());
+        winner.getCardsList().removeFirst();
+        winner.getCardsList().addLast(loser.getCardsList().getFirst());
+        loser.getCardsList().removeFirst();
+
     }
 
     public void menu() {
